1b159c4839b22febbad4c222c488fcdff714333a
Elton Mozart <elton.mozart@gmail.com>
2018-02-09 12:12:27 -0200

    Criação da rota de autenticação do usuário
    Criação da rota de autenticação do usuário

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,3 +1,19 @@
 module.exports.home = function(application, req, res){
     res.render('index');
 }
+
+module.exports.autenticar = function(application, req, res){
+    dodosForm = req.body;
+
+    req.assert('usuario', 'Usuário é obrigatório.').notEmpty();
+    req.assert('senha', 'Usuário é obrigatório.').notEmpty();
+
+    var erros = req.validationErrors();
+
+    if(erros){
+        res.render('index', {validacao: erros});
+        return;
+    }
+
+    res.send('Tudo Ok para iniciar a sessão.');
+}
