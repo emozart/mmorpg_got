3e361a3058725f2e2ef3628e090be4eb654f2a09
Elton Mozart <elton.mozart@gmail.com>
2018-02-11 11:15:38 -0200

    Destruição da sessão
    Destruição da sessão

--- app/controllers/jogo.js
+++ app/controllers/jogo.js
@@ -1,3 +1,13 @@
 module.exports.jogo = function(application, req, res){
-    res.render('jogo');
+    if(req.session.autorizado){
+        res.render('jogo');
+    }else{
+        res.send('Usuário não logado. Faça o login para acessar o jogo.')
+    }
+}
+
+module.exports.sair = function(application, req, res){
+    req.session.destroy(function(err){
+        res.render('index', {validacao: {}});
+    });
 }
